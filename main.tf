cafc255a5cc9f79131bf3b98990c445869c6debb
Nithya Kamalanathan <nithyakamalanathan@Nithyas-Air.fritz.box>
2023-03-13 22:41:30 +0100

    Init Terraform

---
+++ main.tf
@@ -0,0 +1,19 @@
+
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+
+
+
